c80435e64714075bf0d8cc1eaf43fd45b45bc21c
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2023-09-22 00:08:22 +0530

    array1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9a/40d08755ab58001e15cfe8ca3217afee
@@ -0,0 +1,11 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = "Hello";
+	    System.out.println(greeting);
+
+	}
+
+}
---
+++ JavaPractice/src/Array1.java
@@ -0,0 +1,21 @@
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int marks[];
+		marks = new int [5];
+		
+		        marks[0]=20;
+				marks[1]=30;
+				marks[2]=40;
+				marks[3]=60;
+				marks[4]=23;
+				for(int i=0;i<marks.length;i++)
+				{
+					System.out.println(marks[i]);
+				}	
+
+	}
+
+}
